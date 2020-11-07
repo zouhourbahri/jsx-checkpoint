78e5f0c63ffd0aba0a6a8b5eb3868dc176cf3b37
zouhour <zouhour.bahri@yahoo.com>
2020-11-07 13:12:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import reactjsx from "./image/reactjsx.jpg";
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+          <h1 className="title red">REACT LOGO / APPLICATIONS</h1>
+          <br />
+          <img src={reactjsx} alt="" className="image positionLeft" />
+          <br />
+          <img src="./React-websites.png" alt="" className="image positionRight" />
+        </div>
+        <video width={500} height={300} controls>
+          <source src="./video/react-for-beginners-what-is-jsx.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </>
   );
 }
 
